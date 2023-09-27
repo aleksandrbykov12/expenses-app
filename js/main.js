a82043502cf3b5e7b9cd841163167f54652ad1e2
aleksandrbykov12 <136463234+aleksandrbykov12@users.noreply.github.com>
2023-09-27 17:29:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const statusNode = document.getElementById('status');
 const historyNode = document.getElementById('history');
 
 // Global Variables
-const expenses = [];
+let expenses = [];
 
 let limit;
 let sum = 0;
@@ -48,15 +48,38 @@ resetExpenseBtnNode.addEventListener('click', reset);
 // ===============FUNCTION'S===============
 
 init()
+initExpenses()
 
 // Отображение начальных статусов
 function init() {
-    limitNode.innerText = NO_LIMIT;
+    limit = localStorage.getItem('limit');
+
+    if (limit === null) {
+        limitNode.innerText = NO_LIMIT;
+    } else {
+        renderLimit();
+        changeInput();
+    };
+
     totalNode.innerText = `0 ${CURRENCY}`;
     statusNode.innerText = STATUS_IN_LIMIT;
     historyNode.innerText = NO_EXPENSE;
-
     statusNode.style.color = 'green';
+}
+
+function initExpenses() {
+    const expensesFromStorageString = localStorage.getItem('expenses');
+    const expensesFromStorage = JSON.parse(expensesFromStorageString);
+
+    if (Array.isArray(expensesFromStorage)) {
+        expenses = expensesFromStorage;
+    }
+
+    if (!expenses.length) {
+        historyNode.innerText = NO_EXPENSE;
+    } else {
+        renderHistory();
+    }
 }
 
 function addLimitBtnHandler(event) {
@@ -64,8 +87,9 @@ function addLimitBtnHandler(event) {
     limit = getLimit();
 
     if (!limit || limit < 0) {
+        validation(limitFormNode);
         return;
-    }
+    };
 
     renderLimit();
     changeInput();
@@ -77,17 +101,20 @@ function addExpenseBtnHandler(event) {
     expense = getExpense();
 
     if (!expense || expense < 0) {
+        validation(expenseFormNode);
         return;
     }
 
     let currentCategory = getCategory();
 
     if (!currentCategory || currentCategory === 'Выберите категорию') {
+        validation(categoryNode);
         return;
     }
 
     const newExpense = { current: expense, category: currentCategory};
     expenses.push(newExpense);
+    saveExpensesToStorage();
 
     renderHistory();
     calculateTotalExpense();
@@ -96,10 +123,18 @@ function addExpenseBtnHandler(event) {
     checkStatus();
 }
 
+// Сохранение трат в localStorage
+function saveExpensesToStorage() {
+    const expensesString = JSON.stringify(expenses);
+    localStorage.setItem('expenses', expensesString);
+}
+
 // Получние лимита из поля ввода
 function getLimit() {
     limit = parseInt(limitInputNode.value);
     clearInputLimit();
+
+    localStorage.setItem('limit', limit);
 
     return limit;
 }
@@ -193,6 +228,8 @@ function reset() {
     expense = 0;
     statusNode.innerText = STATUS_IN_LIMIT;
     statusNode.style.color = 'green';
+
+    saveExpensesToStorage();
 }
 
 // Проверка статуса 
@@ -204,4 +241,11 @@ function checkStatus() {
         statusNode.innerText = STATUS_IN_LIMIT;
         statusNode.style.color = 'green';
     }
+}
+
+// Добавление анимации при пустом поле ввода
+function validation(node) {
+    node.style.scale = '1.2';
+    node.style.transition = 'all 0.2s linear'
+    setTimeout(() => node.style.scale = '1', 300)
 }
